075c679ac285792c5c7fcf7e507849691d602fb4
wadim55 <korkin_wadim@mail.ru>
2023-06-10 17:40:56 +0600

    1

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,29 @@
+
+using UnityEngine;
+
+public abstract class Item : MonoBehaviour
+{
+    private Transform player;
+
+    public void Awake()
+    {
+        if (GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+       
+    }
+
+
+    public void Start()
+    {
+        ItemEvents.onEnableItem(this.gameObject);
+     
+        if (GameObject.FindGameObjectWithTag("Item") != null)
+        {
+            transform.parent = GameObject.FindGameObjectWithTag("Item").transform;
+        }
+       
+    }
+
+}
---
+++ Assets/Scripts/ItemEvents.cs
@@ -0,0 +1,9 @@
+using System;
+using UnityEngine;
+
+
+public static class ItemEvents
+{
+    public static Action<GameObject> onEnableItem;
+
+}
